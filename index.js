d6f694283cb8bd65617723c17b8426989f83844d
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-08-30 07:42:17 +0530

    redis parser setup

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import net from "net"
+import Parser from "redis-parser"
 
 const server = net.createServer(connections => {
     console.log('Client Connected')
 
-    connections.on("data", data => {
-        console.log("=>", data.toString())
+    const parser = new Parser({
+        returnReply: (reply) => {
+            console.log('=>', reply)
+
+            if (reply[0] && reply[0].toUpperCase() === "PING") {
+                connections.write("+PONG\r\n")
+            } else {
+                connections.write("+OK\r\n")
+            }
+        },
+        returnError: (err) => console.log('=>', err)
     })
 
+    connections.on("data", data => {
+        parser.execute(data)
+    })
 })
 
 server.listen(8000, () => {
-    console.log('custom redis server running of port 8000')
+    console.log('custom redis server running on port 8000')
 })
